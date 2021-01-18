1b5bc0445ff6b72616fdf50d2f84448d57479a47
Egereva Marina <marina.egereva@gmail.com>
2021-01-18 19:30:11 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <button class="btn primary">Показать сообщение</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
